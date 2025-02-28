8430a8588f9c9e0d8a793b5cbd68bb84212af2ea
ghassen ben mahmoud <ghasse.benmahmoud@esprit.tn>
2025-02-28 10:52:00 +0100

    Page Login

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bacasime: ["Bacasime Antique", "serif"],
+        alegreya: ["Alegreya Sans", "sans-serif"],
+        averia: ["Averia Serif Libre", "serif"],
+        alef: ["Alef", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
