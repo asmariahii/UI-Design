7fe3d9923f1cf442299994f3215ef27c194319b6
asma riahi <asma.riahi@esprit.tn>
2025-02-28 20:45:59 +0100

    Page Réclamation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    // Ajoutez ici les chemins vers vos fichiers HTML et JavaScript
-    "./*.{html,js}",
-  ],
-  darkMode: 'class', // Permet de basculer entre les modes en utilisant la classe 'dark'
+  content: ["./*.html"],
   theme: {
-    extend: {
-
-      fontFamily: {
-        alef: ['Alef', 'sans-serif'],
-      },
-      fontSize: {
-        '20': '20px', // Déclare la taille de police 20px
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
+
 
 
