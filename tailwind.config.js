53875d9cd52597ef968d47c90a1aa93140179455
med oussema boussida <medoussemaboussida@gmail.com>
2025-02-28 11:44:45 +0100

    page acceuil

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        bacasime: ["Bacasime Antique", "serif"],
+        alegreya: ["Alegreya Sans", "sans-serif"],
+        averia: ["Averia Serif Libre", "serif"], 
+        alef: ["Alef", "sans-serif"], 
+
+      },
+    },
+  },
+  corePlugins: {
+  },
+  plugins: [],
+}
+
